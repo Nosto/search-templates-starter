d94a4c51416607d1fa9948a9c6c6fdb955535ab8
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-07-28 16:22:27 +0300

    chore: more updates

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,15 +3,14 @@ import Facet from "./Facet"
 import RangeFacet from "./RangeFacet"
 import Icon from "./elements/Icon"
 import { SearchStatsFacet, SearchTermsFacet } from "@nosto/nosto-js/client"
+import styles from "../styles/components/sidebar.module.css"
+import buttonStyles from "../styles/components/button.module.css"
 
 export const toggleButtonId = "toggle-mobile-sidebar"
 
 function ToggleSidebarButton({ className }: { className?: string } = {}) {
   return (
-    <label
-      className={`ns-act-btn ns-close-sidebar ns-absolute ns-d-block ns-d-md-none ns-text-align-right ns-align-items-center ns-clickable ns-font-3 ${className}`}
-      for={toggleButtonId}
-    >
+    <label className={`${buttonStyles.action} ${styles.close} ${className}`} for={toggleButtonId}>
       <Icon name="close" />
     </label>
   )
@@ -22,22 +21,16 @@ export default function SideBar() {
 
   return facets?.length > 0 ? (
     <>
-      <input type="checkbox" id={toggleButtonId} class="ns-facet-sidebar-toggle ns-d-none" />
-      <label
-        class="ns-sidebar-backdrop ns-fixed ns-w-100 ns-h-100 ns-background-black ns-d-none"
-        for={toggleButtonId}
-      />
-      <div
-        class="ns-sidebar-wrapper ns-d-none ns-d-md-block ns-h-100 ns-col-3 ns-col-l-2"
-        style={loading ? "opacity: 0.3;" : ""}
-      >
-        <div class="ns-sidebar-content ns-relative ns-background-white ns-h-100 ns-mr-0 ns-mr-md-auto">
-          <div class="ns-sidebar-header ns-d-flex ns-align-items-baseline ns-p-4">
-            <span class="ns-font-medium ns-font-5 ns-color-black">Filters</span>
+      <input type="checkbox" id={toggleButtonId} className={styles.toggle} />
+      <label className={styles.backdrop} for={toggleButtonId} />
+      <div className={styles.wrapper} style={loading ? "opacity: 0.3;" : ""}>
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <span>Filters</span>
             <ToggleSidebarButton />
           </div>
-          <div class="ns-sidebar-menu">
-            <ul class="ns-list-unstyled ns-p-0 ns-m-0">
+          <div className={styles.facets}>
+            <ul>
               {facets?.map(facet => {
                 switch (facet.type) {
                   case "terms":
