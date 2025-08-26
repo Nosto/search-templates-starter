2d221f1548d956966357c448fda01cc3d9417326
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-08-26 15:39:57 +0300

    fix: config and vi tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,34 @@
 import { resolve } from "path"
 import { defineConfig } from "vitest/config"
 import preact from "@preact/preset-vite"
-import { loadEnv } from "vite"
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "")
-  return {
-    build: {
-      outDir: "dist",
-      minify: true,
-      sourcemap: true,
-      rollupOptions: {
-        input: resolve(__dirname, `src/entries/${env.VITE_MODE}.tsx`),
-        output: {
-          entryFileNames: "[name].js",
-          assetFileNames: "[name][extname]"
-        }
-      }
-    },
-    plugins: [preact()],
-    server: {
-      port: 8000
-    },
-    test: {
-      globals: true,
-      environment: "jsdom"
-    },
-    define: {
-      __MODE__: JSON.stringify(env.VITE_MODE)
-    },
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, "src")
+export default defineConfig(({ mode }) => ({
+  build: {
+    outDir: "dist",
+    minify: true,
+    sourcemap: true,
+    rollupOptions: {
+      input: resolve(__dirname, `src/entries/${mode}.tsx`),
+      output: {
+        entryFileNames: "[name].js",
+        assetFileNames: "[name][extname]"
       }
     }
+  },
+  plugins: [preact()],
+  server: {
+    port: 8000
+  },
+  test: {
+    globals: true,
+    environment: "jsdom"
+  },
+  define: {
+    __MODE__: JSON.stringify(mode)
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
   }
-})
+}))
