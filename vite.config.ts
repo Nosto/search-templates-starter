ad8a2c56a58e8b9123b9e148ee1fdcb8a1e03d58
tomas.jaseliunas <tomas.jaseliunas@searchnode.com>
2025-08-26 15:05:15 +0300

    fix: mode load

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,40 @@
 import { resolve } from "path"
 import { defineConfig } from "vitest/config"
 import preact from "@preact/preset-vite"
+import { loadEnv } from "vite"
 
-export default defineConfig(({ mode }) => ({
-  build: {
-    outDir: "dist",
-    entry: {
-      injected: resolve(__dirname, "src/entries/injected.tsx"),
-      native: resolve(__dirname, "src/entries/native.tsx")
-    },
-    formats: ["es", "cjs"],
-    minify: true,
-    sourcemap: true,
-    rollupOptions: {
-      input: resolve(__dirname, "src/index.tsx"),
-      output: {
-        entryFileNames: "[name].js",
-        assetFileNames: "[name][extname]"
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  return {
+    build: {
+      outDir: "dist",
+      formats: ["es", "cjs"],
+      minify: true,
+      sourcemap: true,
+      rollupOptions: {
+        input: resolve(__dirname, `src/entries/${env.VITE_MODE}.tsx`),
+        output: {
+          entryFileNames: "[name].js",
+          assetFileNames: "[name][extname]"
+        }
       }
-    }
-  },
-  plugins: [preact()],
-  server: {
-    port: 8000
-  },
-  test: {
-    globals: true,
-    environment: "jsdom"
-  },
-  define: {
-    __MODE__: JSON.stringify(mode)
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src")
     },
-    dedupe: ["preact", "preact/hooks"]
+    plugins: [preact()],
+    server: {
+      port: 8000
+    },
+    test: {
+      globals: true,
+      environment: "jsdom"
+    },
+    define: {
+      __MODE__: JSON.stringify(env.VITE_MODE)
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src")
+      },
+      dedupe: ["preact", "preact/hooks"]
+    }
   }
-}))
+})
