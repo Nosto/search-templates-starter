cb2e69ee85eac8dc2980f1d59d5b0cf523f453c4
Timo Westkämper <timo.westkamper@nosto.com>
2025-09-03 08:56:26 +0300

    chore: review fixes

--- src/components/Campaign/Campaign.tsx
+++ src/components/Campaign/Campaign.tsx
@@ -1,9 +1,15 @@
 type CampaignProps = {
+  /** The placement identifier for the campaign. */
   placement: string
+  /** The ID of the product to associate with the campaign. */
   productId?: string
+  /** The variant ID of the product. */
   variantId?: string
+  /** The ID of the template to use for rendering the campaign. If provided, the campaign will be rendered using this template. */
   template?: string
+  /** If set to "false", the component will not automatically load the campaign on connection. Defaults to "true". */
   init?: boolean
+  /** If true, the component will only load the campaign when it comes into view using IntersectionObserver. Defaults to false. */
   lazy?: boolean
 }
 
@@ -12,17 +18,6 @@ type CampaignProps = {
  * This component fetches campaign data from Nosto and injects it into the DOM.
  * It supports both HTML and JSON response modes, allowing for flexible rendering.
  * The placement or id attribute will be used as the identifier of the placement to be fetched.
- *
- * @property {string} placement (or id) - The placement identifier for the campaign.
- * @property {string} productId - The ID of the product to associate with
- * the campaign.
- * @property {string} [variantId] - The variant ID of the product.
- * @property {string} template - The ID of the template to use for rendering
- * the campaign. If provided, the campaign will be rendered using this template.
- * @property {string} [init] - If set to "false", the component will not
- * automatically load the campaign on connection. Defaults to "true".
- * @property {boolean} [lazy] - If true, the component will only load the campaign
- * when it comes into view using IntersectionObserver. Defaults to false.
  */
 export default function Campaign({ productId, variantId, ...props }: CampaignProps) {
   return (
--- src/components/DynamicCard/DynamicCard.tsx
+++ src/components/DynamicCard/DynamicCard.tsx
@@ -1,9 +1,15 @@
 type DynamicCardProps = {
+  /** The product handle to fetch data for. */
   handle: string
+  /** The section to use for rendering the product. section or template is required. */
   section?: string
+  /** The template to use for rendering the product. section or template is required. */
   template?: string
+  /** The variant ID to fetch specific variant data. */
   variantId?: string
+  /** If true, the component will display placeholder content while loading. */
   placeholder?: boolean
+  /** If true, the component will only fetch data when it comes into view. */
   lazy?: boolean
 }
 
@@ -11,13 +17,6 @@ type DynamicCardProps = {
  * A custom element wrapper that renders a product by fetching the markup from Shopify based on the provided handle and template.
  *
  * This component is designed to be used in a Shopify environment and fetches product data dynamically.
- *
- * @property {string} handle - The product handle to fetch data for. Required.
- * @property {string} section - The section to use for rendering the product. section or template is required.
- * @property {string} template - The template to use for rendering the product. section or template is required.
- * @property {string} [variantId] - The variant ID to fetch specific variant data. Optional.
- * @property {boolean} [placeholder] - If true, the component will display placeholder content while loading. Defaults to false.
- * @property {boolean} [lazy] - If true, the component will only fetch data when it comes into view. Defaults to false.
  */
 export default function DynamicCard({ variantId, ...props }: DynamicCardProps) {
   return (
