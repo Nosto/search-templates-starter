639d3ba6bda9cc9b3da83c4713d88a2e9ba64fa0
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-09-25 12:23:54 +0300

    chore: add notice about css module types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,9 @@
-declare module "*.module.css" {
-  const classes: { [key: string]: string }
-  export default classes
-}
+/**
+ * If you are redirected here when you try to import a CSS module with VSCode,
+ * you may want to do this to enable proper CSS type resolution in the IDE:
+ *
+ * https://code.visualstudio.com/docs/typescript/typescript-compiling#_using-the-workspace-version-of-typescript
+ */
+
+declare module "*.module.css"
+declare module "*.module.scss"
