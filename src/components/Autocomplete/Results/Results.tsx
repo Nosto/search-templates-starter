5e94c581e6f9e03b0e11b1733cd762dc3f6c1470
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-09-25 12:19:11 +0300

    feat: add history support

--- src/components/Autocomplete/Results/Results.tsx
+++ src/components/Autocomplete/Results/Results.tsx
@@ -1,25 +1,32 @@
-import { useResponse } from "@nosto/search-js/preact/hooks"
+import { useNostoAppState, useResponse } from "@nosto/search-js/preact/hooks"
 import style from "./Results.module.css"
 import Keywords from "./Keywords"
 import Products from "./Products"
+import { pick } from "@nosto/search-js/utils"
+import { History } from "./History"
 
 type ResultsProps = {
   onSubmit: (query: string) => void
 }
 
 export default function Results({ onSubmit }: ResultsProps) {
+  const { loading, query } = useNostoAppState(state => pick(state, "loading", "query"))
   const { keywords, products } = useResponse()
 
-  if (!keywords?.hits?.length && !products?.hits?.length) {
-    return
+  const hasResults = !!(keywords?.hits?.length || products?.hits?.length)
+  const hasHistory = !!useNostoAppState(state => state.historyItems?.length)
+
+  if (!hasResults && !hasHistory) {
+    return null
   }
 
   return (
     <div className={style.autocomplete} data-nosto-element="autocomplete">
       <div className={`${style.container} ${style.paddingContainer}`}>
         <div className={style.items}>
-          <Keywords keywords={keywords} onSubmit={onSubmit} />
-          <Products products={products} />
+          {hasHistory && <History onSubmit={onSubmit} />}
+          {hasResults && <Keywords keywords={keywords} onSubmit={onSubmit} />}
+          {hasResults && <Products products={products} />}
         </div>
       </div>
     </div>
