bcee8bb415d764a9a8bfa195104541029e1125e1
Timo Westkämper <timo.westkamper@nosto.com>
2025-10-03 09:03:18 +0300

    fix: switch to lazy portal injection

---
+++ src/elements/Portal/Portal.tsx
@@ -0,0 +1,22 @@
+import { waitForElement } from "@/utils/waitForElement"
+import { ComponentChildren } from "preact"
+import { createPortal } from "preact/compat"
+import { useEffect, useState } from "preact/hooks"
+
+type Props = {
+  target: string
+  children: ComponentChildren
+}
+
+export default function Portal({ target, children }: Props) {
+  const [element, setElement] = useState<HTMLElement | null>(document.querySelector<HTMLElement>(target))
+
+  useEffect(() => {
+    if (element) return
+    waitForElement(target).then(el => {
+      setElement(el)
+    })
+  }, [target, element])
+
+  return element ? createPortal(children, element) : null
+}
