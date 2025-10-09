e86e7d628b40a653946a27dfd05da5904f101744
Timo Westkämper <timo.westkamper@nosto.com>
2025-10-09 12:26:27 +0300

    fix: use nosto-image only when available

--- src/components/Product/ProductImage.tsx
+++ src/components/Product/ProductImage.tsx
@@ -2,7 +2,7 @@ import Image from "@/elements/Image/Image"
 import { imageProps } from "./imageProps"
 
 export default function ProductImage({ src, alt }: { src: string; alt?: string }) {
-  if (src.includes("cdn.shopify.com")) {
+  if (src.includes("cdn.shopify.com") && customElements.get("nosto-image")) {
     return <Image src={src} alt={alt} {...imageProps} />
   }
   return <img src={src} alt={alt} />
