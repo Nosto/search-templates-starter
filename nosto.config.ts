9acefeaabadfe3ebfacbec7ab30a2386499278ce
Maija Lintunokka <maija.lintunokka@nosto.com>
2025-10-20 14:42:05 +0300

    chore: add cli config docs

--- nosto.config.ts
+++ nosto.config.ts
@@ -2,13 +2,24 @@ import { build as viteBuild } from "vite"
 import { makeConfig } from "@nosto/nosto-cli"
 import type { Plugin } from "vite"
 
+/**
+ * Nosto CLI configuration file required for communication with the `nosto` CLI tool.
+ * The commands can be updated or reimplemented freely as long as they adhere to the same contracts.
+ */
 export default makeConfig({
+  /**
+   * Perform a single build and exit
+   */
   onBuild: async () => {
     await viteBuild({
       configFile: "vite.config.ts",
       mode: "injected"
     })
   },
+  /**
+   * Build and keep watching files, exiting on user signal (Ctrl+C).
+   * Should invoke onAfterBuild after each successful compile.
+   */
   onBuildWatch: async ({ onAfterBuild }) => {
     await viteBuild({
       configFile: "vite.config.ts",
