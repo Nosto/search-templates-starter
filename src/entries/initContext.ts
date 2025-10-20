2dc39de83d5291378d58f6e56b0cf40221e3f188
Tomas Jaseliūnas <tomas.jaseliunas@Mac.lan>
2025-10-20 11:59:06 +0300

    fix: page type fixes

--- src/entries/initContext.ts
+++ src/entries/initContext.ts
@@ -2,14 +2,18 @@ import { createQueryFromUrl } from "@/mapping/url/createQueryFromUrl"
 import { categoryConfig, serpConfig } from "@/config"
 import { createStore } from "@nosto/search-js/preact/common"
 import { newSearch } from "@nosto/search-js/preact/legacy"
+import { PageType } from "@nosto/nosto-js/client"
 
 type Config = typeof categoryConfig | typeof serpConfig
-type PageType = "search" | "category"
 
 export function initContext(pageType: PageType, config: Config) {
   const store = createStore()
   const query = createQueryFromUrl()
 
+  if (pageType !== "category" && pageType !== "search") {
+    throw new Error(`Unsupported pageType: ${pageType}`)
+  }
+  
   if (query.query || pageType === "category") {
     newSearch(
       {
