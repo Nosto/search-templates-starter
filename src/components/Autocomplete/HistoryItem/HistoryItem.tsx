6ace1e4472ddb51294884f34f499a289f79d07bd
Timo Westkämper <timo.westkamper@nosto.com>
2025-10-23 20:38:27 +0300

    chore: cleanup

--- src/components/Autocomplete/HistoryItem/HistoryItem.tsx
+++ src/components/Autocomplete/HistoryItem/HistoryItem.tsx
@@ -1,7 +1,5 @@
 import { HistoryElement } from "@nosto/search-js/preact/autocomplete"
 import styles from "./HistoryItem.module.css"
-import { cl } from "@nosto/search-js/utils"
-import { useState, useEffect } from "preact/hooks"
 
 type HistoryItemProps = {
   item: string
@@ -9,36 +7,9 @@ type HistoryItemProps = {
 }
 
 export function HistoryItem({ item, onSubmit }: HistoryItemProps) {
-  const [focused, setFocused] = useState(false)
-
-  useEffect(() => {
-    const handleFocusChange = () => {
-      setFocused(document.activeElement?.getAttribute("data-roving-focus-item") === "true")
-    }
-
-    document.addEventListener("focusin", handleFocusChange)
-    document.addEventListener("focusout", handleFocusChange)
-
-    return () => {
-      document.removeEventListener("focusin", handleFocusChange)
-      document.removeEventListener("focusout", handleFocusChange)
-    }
-  }, [])
-
   return (
     <HistoryElement key={item} onSubmit={() => onSubmit(item)}>
-      <div
-        className={cl(styles.container, focused && styles.highlighted)}
-        data-roving-focus-item="true"
-        role="button"
-        tabIndex={0}
-        onClick={() => onSubmit(item)}
-        onKeyDown={e => {
-          if (e.key === "Enter" || e.key === " ") {
-            onSubmit(item)
-          }
-        }}
-      >
+      <div className={styles.container} data-roving-focus-item="true" role="button" tabIndex={0}>
         <div className={styles.name}>{item}</div>
       </div>
     </HistoryElement>
