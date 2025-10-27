75e99167e636ee0e066b0febe5d6bef567a2311a
Timo Westkämper <timo.westkamper@nosto.com>
2025-10-27 22:41:27 +0200

    chore: improve image props

--- src/components/Product/imageProps.ts
+++ src/components/Product/imageProps.ts
@@ -5,9 +5,17 @@ export const imageSizes = `(min-width: 1024px) 25vw,
     (min-width: 375px) 50vw,
     100vw`
 
+const windowWidth = window.innerWidth
+
+const breakpoints = [320, 480, 640, 750, 828, 960, 1024, 1280, 1440, 1600, 1920].filter(
+  bp => bp >= windowWidth / 4 && bp <= windowWidth
+)
+
 export const imageProps = {
   width: 750,
-  sizes: imageSizes
+  sizes: imageSizes,
+  fetchpriority: "high",
+  breakpoints
   // if the product images have a consistent aspect ratio, you can uncomment the line below to set it
   //aspectRatio: 0.75
-}
+} as const
