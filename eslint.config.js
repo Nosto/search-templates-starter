74273b158231177b30a8ec2ce974e42ec61d60cb
Timo Westkämper <timo.westkamper@nosto.com>
2025-11-04 09:18:39 +0200

    fix: upgrade storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import globals from "globals"
 import tseslint from "typescript-eslint"
 import eslintConfigPrettier from "eslint-config-prettier"
@@ -6,41 +9,34 @@ import react from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
 import jsxA11y from "eslint-plugin-jsx-a11y"
 
-export default tseslint.config(
-  { ignores: ["dist", "docs", "build"] },
-  {
-    extends: [...tseslint.configs.recommended],
-    files: ["**/*.{js,ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser
+export default tseslint.config({ ignores: ["dist", "docs", "build"] }, {
+  extends: [...tseslint.configs.recommended],
+  files: ["**/*.{js,ts,tsx}"],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser
+  }
+}, {
+  plugins: {
+    react
+  },
+  extends: [
+    react.configs.flat.recommended,
+    react.configs.flat["jsx-runtime"],
+    reactHooks.configs["recommended-latest"]
+  ],
+  files: ["**/*.{js,ts,tsx}"],
+  settings: {
+    react: {
+      version: "19.0"
     }
+  }
+}, {
+  plugins: {
+    "jsx-a11y": jsxA11y
   },
-  {
-    plugins: {
-      react
-    },
-    extends: [
-      react.configs.flat.recommended,
-      react.configs.flat["jsx-runtime"],
-      reactHooks.configs["recommended-latest"]
-    ],
-    files: ["**/*.{js,ts,tsx}"],
-    settings: {
-      react: {
-        version: "19.0"
-      }
-    }
+  rules: {
+    ...jsxA11y.flatConfigs.recommended.rules
   },
-  {
-    plugins: {
-      "jsx-a11y": jsxA11y
-    },
-    rules: {
-      ...jsxA11y.flatConfigs.recommended.rules
-    },
-    files: ["**/*.{js,ts,tsx}"]
-  },
-  eslintConfigPrettier,
-  eslintPluginPrettierRecommended
-)
+  files: ["**/*.{js,ts,tsx}"]
+}, eslintConfigPrettier, eslintPluginPrettierRecommended, storybook.configs["flat/recommended"]);
