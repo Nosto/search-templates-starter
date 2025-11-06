6fd2e0ee5f9d2ab4fe6a163f77ee7da2843aa5ee
Timo Westkämper <timo.westkamper@nosto.com>
2025-11-06 18:14:07 +0200

    fix: add skeleton styles to DynamicCardProduct

--- src/components/Product/DynamicCardProduct.tsx
+++ src/components/Product/DynamicCardProduct.tsx
@@ -2,8 +2,26 @@ import { Product } from "@/types"
 import { SerpElement } from "@nosto/search-js/preact/serp"
 import styles from "./Product.module.css"
 import DynamicCard from "@/elements/DynamicCard/DynamicCard"
+import { cl } from "@nosto/search-js/utils"
 
 export default function DynamicCardProduct({ product }: { product: Product }) {
+  const isSkeleton = product.tags1?.includes("skeleton")
+
+  if (isSkeleton) {
+    return (
+      <div className={cl(styles.container, styles.skeleton)}>
+        <div className={styles.image}>
+          <img src={product.imageUrl} alt={product.name} className={styles.img} />
+        </div>
+        <div className={styles.info}>
+          <div>...</div>
+          <div>...</div>
+          <div>...</div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <SerpElement
       as={DynamicCard}
