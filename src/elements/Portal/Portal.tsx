b16560c597a92cd41f591a2cc16ce635e0b540ec
Timo Westkämper <timo.westkamper@nosto.com>
2025-11-07 10:27:36 +0200

    chore: review fix

--- src/elements/Portal/Portal.tsx
+++ src/elements/Portal/Portal.tsx
@@ -1,7 +1,7 @@
 import { waitForElement } from "@/utils/waitForElement"
 import { ComponentChildren } from "preact"
 import { createPortal } from "preact/compat"
-import { useEffect, useState } from "preact/hooks"
+import { useEffect, useRef, useState } from "preact/hooks"
 
 type Props = {
   target: string
@@ -10,25 +10,19 @@ type Props = {
 }
 
 export default function Portal({ target, clear, children }: Props) {
-  const [element, setElement] = useState<HTMLElement | null>(() => {
-    const el = document.querySelector<HTMLElement>(target)
-    return clear ? cleared(el) : el
-  })
+  const [element, setElement] = useState<HTMLElement | null>(() => document.querySelector<HTMLElement>(target))
+  const isCleared = useRef(false)
 
   useEffect(() => {
     if (!element) {
-      waitForElement(target).then(el => {
-        setElement(clear ? cleared(el) : el)
-      })
+      waitForElement(target).then(setElement)
     }
-  }, [target, clear, element])
+  }, [target, element])
+
+  if (element && clear && !isCleared.current) {
+    element.innerHTML = ""
+    isCleared.current = true
+  }
 
   return element ? createPortal(children, element) : null
-}
-
-function cleared(element: HTMLElement | null) {
-  if (element) {
-    element.innerHTML = ""
-  }
-  return element
 }
